79f2d388277d5f770d3680d3a11b7496565ee852
Sergey Shemyatovsky <sergey.shemyatovsky@ladyluckgames.io>
2020-05-10 03:12:45 +0300

    Added bricks array Added ball bricks collision

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const ball = document.querySelector("#ball");
 const paddle = document.querySelector("#paddle");
 
 const startBtn = document.querySelector(".start");
-const gameOverBtn = document.querySelector(".gameOver");
+const gameOverTxt = document.querySelector(".gameOver");
 
 let livesText = document.querySelector(".lives");
 let scoreText = document.querySelector(".score");
@@ -50,14 +50,42 @@ function keyUpHandler(e) {
 
 function startGame() {
   if (gameOver) {
-    gameOverBtn.style.display = "none";
+    gameOverTxt.style.display = "none";
     ball.style.display = "block";
     lives = 3;
+    setupBricks(16);
     lifeUpdate();
     animationRepeat = requestAnimationFrame(update);
     gameOver = false;
     gameStarted = false;
   }
+}
+
+function setupBricks(num) {
+  var row = {
+    x: (containerDim.width % 100) / 2,
+    y: 50,
+  };
+  for (var x = 0; x < num; x++) {
+    if (row.x > containerDim.width - 100) {
+      row.y += 70;
+      row.x = (containerDim.width % 100) / 2;
+    }
+    brickMaker(row);
+    row.x += 100;
+  }
+}
+
+function brickMaker(row) {
+  let div = document.createElement("div");
+  div.setAttribute("class", "brick");
+  div.style.backgroundColor = "black";
+  let pointDiv = Math.ceil(Math.random() * 10) + 2;
+  div.dataset.points = pointDiv;
+  div.innerHTML = pointDiv;
+  div.style.left = row.x + "px";
+  div.style.top = row.y + "px";
+  container.appendChild(div);
 }
 
 function update() {
@@ -85,7 +113,7 @@ function update() {
 
 function ballOnPaddle() {
   ball.style.top = paddle.offsetTop - 22 + "px";
-  ball.style.left = paddle.offsetLeft + 70 + "px";
+  ball.style.left = paddle.offsetLeft + 90 + "px";
 }
 
 function ballMoving() {
@@ -109,6 +137,21 @@ function ballMoving() {
     ballDir.y *= -1;
   }
 
+  let tempBricks = document.querySelectorAll(".brick");
+
+  if (tempBricks.length == 0) {
+    stopper();
+    setupBricks(20);
+  }
+
+  for (let tarBrick of tempBricks) {
+    if (isCollide(tarBrick, ball)) {
+      ballDir.y *= -1;
+      tarBrick.parentNode.removeChild(tarBrick);
+      scoreUpdate(tarBrick.dataset.points);
+    }
+  }
+
   x += ballDir.x;
   y += ballDir.y;
 
@@ -128,8 +171,23 @@ function lifeUpdate() {
   livesText.innerText = lives;
 }
 
+function scoreUpdate(num) {
+  score += parseInt(num);
+  scoreText.innerText = score;
+}
+
+function lostGame() {
+  gameOverTxt.style.display = "block";
+  gameOverTxt.innerHTML = "YOU LOST";
+  gameOver = true;
+  ball.style.display = "none";
+}
+
 function BallOffScreen() {
   lives--;
+  if (lives === 0) {
+    lostGame();
+  }
   lifeUpdate();
   stopper();
 }
